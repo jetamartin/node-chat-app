f8a65826dae118ef9c1032e04d632e904c920dad
Jet Martin <jetamartin@gmail.com>
2018-09-21 12:22:02 -0700

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
+// Node Module that allows you to create a 'direct' path w/o '../' appearing in address
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 // Setup for Heroku -- if env variable present use it otherwise use 3000
 const port = process.env.PORT || 3000;
-
+// Express middleware express.static required to allow you to display static content
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  })
 })
 
-module.exports = {app};
+// Note calling app.listen actually creates a server
+server.listen(port, () => {
+  console.log(`Started on port ${port}`);
+})
