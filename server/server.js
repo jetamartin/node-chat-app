f246376464299943a4194f250e139254e7777e85
Jet Martin <jetamartin@gmail.com>
2018-09-23 15:56:46 -0700

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage} = require('./utils/message');
+const {generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 
@@ -44,17 +45,9 @@ io.on('connection', (socket) => {
       callback('This is from the server');
   });
 
-    // Code below would transmit message to every user but the sending user's socket
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
-// });
-
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  })
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var from = 'Jet';
@@ -7,6 +7,17 @@ describe('generateMessage', () => {
     var message = generateMessage(from, text);
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
-  })
+  });
+});
 
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Jet'
+    var longitude = 1;
+    var latitude = -2;
+    var url = 'https://www.google.com/maps?q=-2,1'
+    var location = generateLocationMessage(from, latitude, longitude);
+    expect(location.createdAt).toBeA('number');
+    expect(location).toInclude({from, url});
+  })
 })
