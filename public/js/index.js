31cd09e634a5bbb932f8a5e1aceefa4360bdf02c
Jet Martin <jetamartin@gmail.com>
2018-09-24 17:31:45 -0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,66 @@
 var socket = io();
 
+/*
+  If the number of messages is large enough so as to not all be visible on screen
+  at the same time we want to auto scroll the user to the bottom of the screen so
+  that they can see new incoming messages without having to manually scroll to them.
+  However, if the user is looking back through older messages you don't want to
+  auto scroll them to the bottom of the screen if a new message arrives while they
+  are looking at old messages. That would be annoying.
+
+  So the solution is to autoscroll to the bottom only if they are near the bottom
+  of the screen (e.g., last message is not entirely in view).
+
+  To calculate whether the we should scroll the user we need the folloiwing Heights
+   - clientHeight - size of the physical viewable message window
+   - scrollHeight - entire height of all messsages combined including those that
+                    aren't viewable on screen at any one time.
+   - scrollTop    - How far (in pixels) we've scrolled down into the messages.
+
+   Note: because the last message (newMessage) has already been added by the
+    time this function is called we will need to include the height of this message
+
+    We also need to add in the height of the previously last message (lastMessageHeight)
+    for the folloiwing reasons:
+
+      1. for most of the cases, it scrolls down but sometimes it does not, meaning
+       it is not consistent without the lastMessageHeight.
+
+     2. it will neverscroll if you are viewing the second last message unless you
+       add in lastMessageHeight.
+
+*/
+
+function scrollToBottom () {
+  // **** Selectors Variables ******
+  var messages = jQuery('#messages')
+  // newMessage is selector for brand new message that was just added
+  // We get the last item by using 'li:lastChild' selector
+  var newMessage = messages.children('li:last-child');
+
+  // innerHeight get height of item plus any padding we've applied via css
+  var newMessageHeight = newMessage.innerHeight();
+
+  // We need height of previously last message using .prev(). Moves you to previous to last item
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  // ***** Heights Variables *****
+
+  // Height of view window
+  var clientHeight = messages.prop('clientHeight');
+
+  // Total size of the messages (This Height may be much larger than view window)
+  var scrollHeight = messages.prop('scrollHeight');
+
+  // Distance user is scrolled down from the top
+ var scrollTop = messages.prop('scrollTop');
+
+ if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+   messages.scrollTop(scrollHeight);
+ }
+
+}
+
 // 'connect' built in event listener for connection
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -17,6 +78,7 @@ socket.on('newMessage', function(message) {
 
   jQuery('#messages').append(html);
 
+  scrollToBottom();
 });
 
 // Display newLocationMessage from Server
@@ -30,6 +92,8 @@ socket.on('newLocationMessage', function (message) {
     url: message.url,
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
+
 
   // Prior to use of Mustache Templates HTML was to add via jQuery - see lines below
   // var li = jQuery('<li></li>');
